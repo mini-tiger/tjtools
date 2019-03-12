24c113dd5818ae14b8481e0cafc476191d4aeabd
tao-work-centos6.x <61566027@163.com>
2019-03-12 12:16:54 +0800

    ubs log modify

---
+++ utils/当前执行文件路径.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"os/exec"
+	"os"
+	"path/filepath"
+)
+
+/*获取当前文件执行的路径*/
+func GetCurPath() string {
+	file, _ := exec.LookPath(os.Args[0])
+
+	//得到全路径，比如在windows下E:\\golang\\test\\a.exe
+	path, _ := filepath.Abs(file)
+
+	rst := filepath.Dir(path)
+
+	return rst
+}
