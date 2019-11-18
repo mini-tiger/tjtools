96750690fcc95665d16162a695ad31b845332321
tj-thinkpad <61566027@163.com>
2019-11-18 18:03:43 +0800

    work

--- file/遍历.go
+++ file/遍历.go
@@ -38,14 +38,14 @@ import (
 
 
 
-//var filesPool = sync.Pool{
-//	New: func() interface{} {
-//		b := make([]string, 0)
-//		return &b
-//	},
-//}
+var filesPool = sync.Pool{
+	New: func() interface{} {
+		b := make([]string, 0)
+		return &b
+	},
+}
 
-func GetFileList(path string,filesPool sync.Pool) (*[]string, error) {
+func GetFileList(path string) (*[]string, error) {
 	//files := make([]string, 0)
 	files := filesPool.Get().(*[]string)
 	f, err := os.Stat(path)
