317a1b91cc9d1a9be6419bf14473eb923cfb0532
tao-work-centos6.x <61566027@163.com>
2019-12-20 22:29:29 +0800

    work

--- db/oracle/CheckOracleConn.go
+++ db/oracle/CheckOracleConn.go
@@ -5,13 +5,13 @@ import (
 	"os"
 )
 
-func CheckOracleConn(dsn string) error {
+func CheckOracleConn(dsn *string) error {
 	os.Setenv("NLS_LANG", "")
 	//if len(os.Args) != 2 {
 	//	log.Fatalln(os.Args[0] + " user/password@host:port/sid")
 	//}
 
-	db, err := sql.Open("oci8", dsn)
+	db, err := sql.Open("oci8", *dsn)
 	//fmt.Printf("%+v\n",db)
 	if err != nil {
 		return err
