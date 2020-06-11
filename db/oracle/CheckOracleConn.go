19381b98d550c927d8be87b16816b155fd19f49c
tj-thinkpad <61566027@163.com>
2020-06-11 13:54:20 +0800

    work

--- db/oracle/CheckOracleConn.go
+++ db/oracle/CheckOracleConn.go
@@ -39,3 +39,21 @@ func CheckOracleConn(dsn *string) (err error) {
 	}()
 	return nil
 }
+
+func CheckOracleLive(db *sql.DB) (err error) {
+	var rows *sql.Rows
+	// 每次生成的内存地址不同，不能重复利用
+	defer func() {
+		rows = nil
+	}()
+
+	rows, err = db.Query("select 3.14 from dual")
+	if err != nil {
+		return err
+	}
+	//fmt.Println(rows.Next())
+	defer func() {
+		_ = rows.Close()
+	}()
+	return nil
+}
