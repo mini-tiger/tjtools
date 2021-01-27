43305176f285877d0fc6455f9db2f8f88e533261
taojun <61566027@163.com>
2021-01-27 13:14:39 +0800

    Default Changelist

--- file/checkDirPerm.go
+++ file/checkDirPerm.go
@@ -7,13 +7,11 @@ import (
 )
 
 func CheckDirPerm(filePath string) (err error) {
-
 	var file os.FileInfo
 	file, err = os.Stat(filePath)
 	if err != nil {
 		return
 	}
-
 	s := file.Sys().(*syscall.Stat_t)
 
 	if err = os.Chown(filePath, int(s.Uid), int(s.Gid)); err != nil {
